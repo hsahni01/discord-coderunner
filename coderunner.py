7e756ef7ffc4446d7fa714cc79123b8789f74613
hsahni01 <62819816+hsahni01@users.noreply.github.com>
2020-11-03 08:13:40 -0800

    New Update
    Added, error handling, added pip installation!

--- coderunner.py
+++ coderunner.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import sys
+import os
 
 
 from io import StringIO  # Python3
@@ -17,13 +18,30 @@ async def on_ready():
 
 @bot.command()
 async def run(ctx, *, args):
+	try:
+		old_stdout = sys.stdout
+		result = StringIO()
+		sys.stdout = result
+		exec(args)
+		sys.stdout = old_stdout 
+		result_string = result.getvalue()
+		await ctx.send(str(result_string))
+	except Exception as e:
+		await ctx.send(e)
 
-	old_stdout = sys.stdout
-	result = StringIO()
-	sys.stdout = result
-	exec(args)
-	sys.stdout = old_stdout 
-	result_string = result.getvalue()
-	await ctx.send(str(result_string))
+
+@bot.command()
+async def install(ctx, package):
+	cmd = str(os.system("pip install "+package))
+	try:
+		old_stdout = sys.stdout
+		result = StringIO()
+		sys.stdout = result
+		exec(cmd)
+		sys.stdout = old_stdout 
+		result_string = result.getvalue()
+		await ctx.send("package sent for installation, if it was valid it installed.")
+	except Exception as e:
+		await ctx.send("package sent for installation")
 
 bot.run('DISCORD-TOKEN')
